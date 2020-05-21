f8afd1c66e2b9b23e80d4878eaccb9d9de122ec5
Alex Hurka <alexander.hurka@canada.ca>
2020-05-21 15:57:21 +0000

    Unpack FAQ page woudc-blurb components

--- pages/about/faq.vue
+++ pages/about/faq.vue
@@ -2,78 +2,52 @@
   <v-layout justify-center column align-content-center>
     <h1>{{ $t('about.faq.title') }}</h1>
     <p>{{ $t('about.faq.blurb') }}</p>
-    <div v-for="(question, i) in questions" :key="i">
-      <h4>{{ question.text }}</h4>
-      <woudc-blurb :items="question.answer" />
+    <div v-for="index in $t('about.faq.questions').length" :key="index">
+      <h4 v-text="$t('about.faq.questions[' + (index - 1) + '].text')" />
+      <i18n :path="'about.faq.questions[' + (index - 1) + '].answer'" tag="p">
+        <template v-slot:policy>
+          <nuxt-link :to="localePath('about-datapolicy')" v-text="$t('about.faq.substitutions.policy')" />
+        </template>
+        <template v-slot:submission>
+          <nuxt-link :to="localePath('contributors-submission')" v-text="$t('about.faq.substitutions.submission')" />
+        </template>
+        <template v-slot:registration>
+          <nuxt-link :to="localePath('contributors-registration')" v-text="$t('about.faq.substitutions.registration')" />
+        </template>
+        <template v-slot:access>
+          <nuxt-link :to="localePath('about-dataaccess')" v-text="$t('about.faq.substitutions.access')" />
+        </template>
+        <template v-slot:search>
+          <nuxt-link :to="localePath('data-explore')" v-text="$t('about.faq.substitutions.search')" />
+        </template>
+        <template v-slot:products>
+          <nuxt-link :to="localePath('data-products')" v-text="$t('about.faq.substitutions.products')" />
+        </template>
+        <template v-slot:contributors>
+          <nuxt-link :to="localePath('contributors')" v-text="$t('about.faq.substitutions.contributors')" />
+        </template>
+        <template v-slot:accessibility>
+          <a :href="accessibilityURL" target="_blank" v-text="$t('about.faq.substitutions.accessibility')" />
+        </template>
+        <template v-slot:usability>
+          <a :href="usabilityURL" target="_blank" v-text="$t('about.faq.substitutions.usability')" />
+        </template>
+        <template v-slot:w3c>
+          <a :href="w3cURL" target="_blank" v-text="$t('about.faq.substitutions.w3c')" />
+        </template>
+      </i18n>
     </div>
   </v-layout>
 </template>
 
 <script>
-import WoudcBlurb from '~/components/WoudcBlurb'
-
-function makeLink(text, to, external) {
-  const link = { text, to }
-  if (external) {
-    link.type = 'external'
-  }
-
-  return { link }
-}
-
-
 export default {
-  components: {
-    'woudc-blurb': WoudcBlurb
-  },
   data() {
-    // (Deep-ish) copy list of questions from translations.
-    const questions = this.$t('about.faq.questions').map((entry) => {
-      return {
-        text: entry.text,
-        answer: Array.isArray(entry.answer) ? [...entry.answer] : entry.answer
-      }
-    })
-
-    // Add parts of the answer text that are independent of language
-    // (e.g. some link endpoints)
-    questions[0].answer[1] = makeLink(questions[0].answer[1], 'about-datapolicy', false)
-    questions[0].answer.push('.')
-
-    questions[1].answer[1] = makeLink(questions[1].answer[1], 'contributors-submission', false)
-    questions[1].answer[3] = makeLink(questions[1].answer[3], 'contributors-registration', false)
-    questions[1].answer.push('.')
-
-    questions[2].answer[1] = makeLink(questions[2].answer[1], 'about-dataaccess', false)
-
-    questions[3].answer[1] = makeLink(questions[3].answer[1], 'data-explore', false)
-    questions[3].answer[3] = makeLink(questions[3].answer[3], 'data-products', false)
-
-    questions[4].answer[1] = makeLink(questions[4].answer[1], 'contributors', false)
-    questions[4].answer.push('.')
-
-    questions[7].answer[1] = makeLink(questions[7].answer[1], 'about-dataaccess', false)
-
-    questions[8].answer[1] = makeLink(
-      questions[8].answer[1],
-      'https://www.tbs-sct.gc.ca/ws-nw/wa-aw/index-eng.asp',
-      true
-    )
-    questions[8].answer[3] = makeLink(
-      questions[8].answer[3],
-      'https://www.tbs-sct.gc.ca/ws-nw/wu-fe/index-eng.asp',
-      true
-    )
-    questions[8].answer[5] = {
-      italic: makeLink(
-        questions[8].answer[5],
-        'https://www.w3.org/WAI/intro/wcag',
-        true
-      )
+    return {
+      accessibilityURL: 'https://www.tbs-sct.gc.ca/ws-nw/wa-aw/index-eng.asp',
+      usabilityURL: 'https://www.tbs-sct.gc.ca/ws-nw/wu-fe/index-eng.asp',
+      w3cURL: 'https://www.w3.org/WAI/intro/wcag'
     }
-    questions[8].answer.push('.')
-
-    return { questions }
   },
   nuxtI18n: {
     paths: {
