fe39f7576d46a94a8e300692ec059d06e3b92e01
danielwaiforssell <51061073+danielwaiforssell@users.noreply.github.com>
2021-02-08 11:16:47 -0500

    Error Page Rework Pull Request (#31)
    * Moved Blurb and Header sections on various pages out of the row and col tags
    
    * News Items now come from woudc API rather than wfs layer request
    
    * Removed form from WOUDC ui contact page
    
    * reworked 404 and other error pages
    
    * transferred error page translations to i18n and modified margins/spacing
    
    * Removed personally from line 800
    
    * Removed personnellement and lowercase service
    
    * Fixed email to woudc link

--- layouts/error.vue
+++ layouts/error.vue
@@ -1,14 +1,65 @@
 <template>
   <v-app>
-    <h1 v-if="error.statusCode === 404">
-      {{ pageNotFound }}
-    </h1>
-    <h1 v-else>
-      {{ otherError }}
-    </h1>
-    <NuxtLink to="/">
-      Home page
-    </NuxtLink>
+    <v-container v-if="error.statusCode === 404" class="mt-3">
+      <v-row class="d-flex justify-center">
+        <v-col cols="12" md="6" xs="3" xl="3" lg="4">
+          <div style="display: flex">        
+            <v-icon color="red" class="d-flex align-start mt-1"> 
+              mdi-alert 
+            </v-icon>
+            <h2 v-t="{path: 'error.not-found', locale: 'en'}" class="mx-1" />
+          </div>
+          <p v-t="{path: 'error.error-text.not-found-text', locale: 'en'}" />
+          <div style="display: flex">
+            <li v-t="{path: 'error.error-link-start', locale: 'en'}" class="ml-10" />
+            <a v-t="{path: 'error.error-link-end', locale: 'en'}" :href="baseURL" class="ml-1" />
+          </div>
+        </v-col>
+        <v-col cols="12" md="6" sm="12" xl="3" lg="4" justify="center">
+          <div style="display: flex">
+            <v-icon color="red" class="d-flex align-start mt-1">
+              mdi-alert
+            </v-icon>
+            <h2 v-t="{path: 'error.not-found', locale: 'fr'}" class="mx-1" />
+          </div>
+          <p v-t="{path: 'error.error-text.not-found-text', locale: 'fr'}" />
+          <div style="display: flex">
+            <li v-t="{path: 'error.error-link-start', locale: 'fr'}" class="ml-10" />
+            <a v-t="{path: 'error.error-link-end', locale: 'fr'}" :href="baseURL" class="ml-1" />
+          </div>
+        </v-col>
+      </v-row>
+    </v-container>
+    <v-container v-else class="mt-3">
+      <v-row class="d-flex justify-center">
+        <v-col cols="12" md="6" sm="12" xl="3" lg="4">
+          <div style="display: flex">
+            <v-icon color="red" class="d-flex align-start mt-1">
+              mdi-alert-circle
+            </v-icon>
+            <h2 v-t="{path: 'error.other-error', locale: 'en'}" class="mx-1" />
+          </div>
+          <p v-t="{path: 'error.error-text.other-error-text', locale: 'en'}" /> 
+          <div style="display: flex">
+            <li v-t="{path: 'error.error-link-start', locale: 'en'}" class="ml-10" />
+            <a v-t="{path: 'error.error-link-end', locale: 'en'}" :href="baseURL" class="ml-1" />
+          </div>
+        </v-col>
+        <v-col cols="12" md="6" sm="12" xl="3" lg="4">
+          <div style="display: flex">
+            <v-icon color="red" class="d-flex align-start mt-1">
+              mdi-alert-circle
+            </v-icon>
+            <h2 v-t="{path: 'error.other-error', locale: 'fr'}" class="mx-1" />
+          </div>
+          <p v-t="{path: 'error.error-text.other-error-text', locale: 'fr'}" />
+          <div style="display: flex">
+            <li v-t="{path: 'error.error-link-start', locale: 'fr'}" class="ml-10" />
+            <a v-t="{path: 'error.error-link-end', locale: 'fr'}" :href="baseURL" class="ml-1" />
+          </div>
+        </v-col>
+      </v-row>
+    </v-container>
   </v-app>
 </template>
 
@@ -23,15 +74,20 @@ export default {
   },
   data() {
     return {
-      pageNotFound: '404 Not Found',
-      otherError: 'An error occurred'
+      baseURL: process.env.BASE_URL
     }
   },
   head() {
     const title =
-      this.error.statusCode === 404 ? this.pageNotFound : this.otherError
+      this.error.statusCode === 404 ? this.pageNotFoundEn : this.otherErrorEn
     return {
       title
+    }
+  },
+  nuxtI18n: {
+    paths: {
+      en: '/error',
+      fr: 'erreur'
     }
   }
 }
