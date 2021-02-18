a85848b9b288d5dd943da1a2961de64c1a3fd23f
danielwaiforssell <daniel.waiforssell@mail.utoronto.ca>
2021-02-18 13:51:20 -0500

    reworked vertical menu layout to horizontal
    merge commit

--- layouts/default.vue
+++ layouts/default.vue
@@ -38,6 +38,7 @@
       </v-list>
     </v-navigation-drawer>
     <v-app-bar fixed app>
+<<<<<<< HEAD
       <div v-if="$vuetify.breakpoint.mdAndUp" class="text-center">
         <v-menu v-for="(group, groupTag) in links" :key="groupTag" offset-y open-on-hover transition="slide-y-transition">
           <template v-slot:activator="{ on, attrs }">
@@ -46,6 +47,16 @@
               {{ $t('banner.' + groupTag) }}
             </v-btn>
             <v-btn v-else elevation="0" :to="localePath(group.link)" nuxt>
+=======
+      <div class="text-center" v-if="$vuetify.breakpoint.mdAndUp">
+        <v-menu offset-y open-on-hover transition="slide-y-transition" v-for="(group, groupTag) in links" :key="groupTag">
+          <template v-slot:activator="{ on, attrs }">
+            <v-btn elevation="0" v-bind="attrs" v-on="on" v-if="group.sections" :to="localePath(group.link)" nuxt>
+              <v-icon>{{ group.icon }}</v-icon>
+              {{ $t('banner.' + groupTag) }}
+            </v-btn>
+            <v-btn elevation="0" v-else :to="localePath(group.link)" nuxt>
+>>>>>>> 57e7920... reworked vertical menu layout to horizontal
               <v-icon>{{ group.icon }}</v-icon>
               {{ $t('banner.' + groupTag) }}
               <v-icon>mdi-chevron-down</v-icon>
