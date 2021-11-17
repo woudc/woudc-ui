a6f328d9b53b7040d6f60b22ca2f022bd314cf22
Kevin Ngai <kevin.ngai@canada.ca>
2021-11-17 10:54:55 -0800

    npm update eslint and jest

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,19 +8,19 @@ module.exports = {
     parser: 'babel-eslint'
   },
   extends: [
-    "plugin:jest/recommended",
-    "plugin:vue/recommended",
-    "eslint:recommended",
-    "prettier/vue",
-    "plugin:prettier/recommended"
+    'plugin:jest/recommended',
+    'plugin:vue/recommended',
+    'eslint:recommended',
+    'prettier/vue',
+    'plugin:prettier/recommended'
   ],
   // add your custom rules here
   rules: {
-    'indent': ['error', 2],
-    'semi': ['error', 'never'],
-    "vue/component-name-in-template-casing": ["error", "PascalCase"],
-    "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off"
+    indent: ['error', 2],
+    semi: ['error', 'never'],
+    'vue/component-name-in-template-casing': ['error', 'PascalCase'],
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
   },
   globals: {
     $nuxt: true // prevent "no-undefined variables" when accessing $nuxt
