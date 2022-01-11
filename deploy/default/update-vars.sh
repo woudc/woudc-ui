8b90a5991c79fb024209e0f6658dab732e33fc43
Kevin Ngai <kevin.ngai@canada.ca>
2022-01-11 09:31:39 -0800

    Fix sed with triple backslash

--- deploy/default/update-vars.sh
+++ deploy/default/update-vars.sh
@@ -7,9 +7,9 @@ BASEDIR=/web/woudc-ui
 #Escaped characters in state.js
 for ui_file in `find $BASEDIR/dist -type f -name "state.js"`
 do
-    sed -i "s#https:\u002F\u002Fbeta-woudc-dev.cmc.ec.gc.ca#$WOUDC_UI_BASE_URL#g" $ui_file
-    sed -i "s#https:\u002F\u002Fgeobeta-woudc-dev.cmc.ec.gc.ca/oapi#$WOUDC_UI_API_URL#g" $ui_file
-    sed -i "s#https:\u002F\u002Fbeta-woudc-dev.cmc.ec.gc.ca#$WOUDC_UI_WAF_URL#g" $ui_file
+    sed -i "s#https:\\\u002F\\\u002Fbeta-woudc-dev.cmc.ec.gc.ca#$WOUDC_UI_BASE_URL#g" $ui_file
+    sed -i "s#https:\\\u002F\\\u002Fbeta-woudc-dev.cmc.ec.gc.ca/oapi#$WOUDC_UI_API_URL#g" $ui_file
+    sed -i "s#https:\\\u002F\\\u002Fbeta-woudc-dev.cmc.ec.gc.ca#$WOUDC_UI_WAF_URL#g" $ui_file
 done
 
 #Other compiled .js files and the 200.html
