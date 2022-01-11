0b73c1eaabe9dde01f7d65c7fa638bdaa0a7fcf7
Kevin Ngai <kevin.ngai@canada.ca>
2022-01-11 11:10:45 -0800

    Fix sed for /waf and /oapi

--- deploy/default/update-vars.sh
+++ deploy/default/update-vars.sh
@@ -8,8 +8,8 @@ BASEDIR=/web/woudc-ui
 for ui_file in `find $BASEDIR/dist -type f -name "state.js"`
 do
     sed -i "s#https:\\\u002F\\\u002Fbeta-woudc-dev.cmc.ec.gc.ca#$WOUDC_UI_BASE_URL#g" $ui_file
-    sed -i "s#https:\\\u002F\\\u002Fgeobeta-woudc-dev.cmc.ec.gc.ca/oapi#$WOUDC_UI_API_URL#g" $ui_file
-    sed -i "s#https:\\\u002F\\\u002Fbeta-woudc-dev.cmc.ec.gc.ca#$WOUDC_UI_WAF_URL#g" $ui_file
+    sed -i "s#https:\\\u002F\\\u002Fgeobeta-woudc-dev.cmc.ec.gc.ca\\\u002Foapi#$WOUDC_UI_API_URL#g" $ui_file
+    sed -i "s#https:\\\u002F\\\u002Fbeta-woudc-dev.cmc.ec.gc.ca\\\u002Farchive#$WOUDC_UI_WAF_URL#g" $ui_file
 done
 
 #Other compiled .js files and the 200.html
@@ -17,5 +17,6 @@ for ui_file in `find $BASEDIR/dist -type f -name "*.js" -o -name "200.html"`
 do
     sed -i "s#https://beta-woudc-dev.cmc.ec.gc.ca#$WOUDC_UI_BASE_URL#g" $ui_file
     sed -i "s#https://geobeta-woudc-dev.cmc.ec.gc.ca/oapi#$WOUDC_UI_API_URL#g" $ui_file
-    sed -i "s#https://beta-woudc-dev.cmc.ec.gc.ca#$WOUDC_UI_WAF_URL#g" $ui_file
+    sed -i "s#https://beta-woudc-dev.cmc.ec.gc.ca/archive#$WOUDC_UI_WAF_URL#g" $ui_file
 done
+
