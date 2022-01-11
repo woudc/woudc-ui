249e56cf23d09260a83c76811d432847170bb990
Kevin Ngai <kevin.ngai@canada.ca>
2022-01-11 09:06:56 -0800

    Comments

--- deploy/default/update-vars.sh
+++ deploy/default/update-vars.sh
@@ -4,6 +4,7 @@ BASEDIR=/web/woudc-ui
 
 . $BASEDIR/.env
 
+#Escaped characters in state.js
 for ui_file in `find $BASEDIR/dist -type f -name "state.js"`
 do
     sed -i "s#https:\u002F\u002Fbeta-woudc-dev.cmc.ec.gc.ca#$WOUDC_UI_BASE_URL#g" $ui_file
@@ -11,6 +12,7 @@ do
     sed -i "s#https:\u002F\u002Fbeta-woudc-dev.cmc.ec.gc.ca#$WOUDC_UI_WAF_URL#g" $ui_file
 done
 
+#Other compiled .js files and the 200.html
 for ui_file in `find $BASEDIR/dist -type f -name "*.js" -o -name "200.html"`
 do
     sed -i "s#https://beta-woudc-dev.cmc.ec.gc.ca#$WOUDC_UI_BASE_URL#g" $ui_file
