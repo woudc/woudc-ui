aa19c0fff143240a7432b21b2da4a34b08e3b565
Kevin Ngai <kevin.ngai@canada.ca>
2022-01-11 10:58:48 -0800

    Fix sed link

--- deploy/default/update-vars.sh
+++ deploy/default/update-vars.sh
@@ -8,7 +8,7 @@ BASEDIR=/web/woudc-ui
 for ui_file in `find $BASEDIR/dist -type f -name "state.js"`
 do
     sed -i "s#https:\\\u002F\\\u002Fbeta-woudc-dev.cmc.ec.gc.ca#$WOUDC_UI_BASE_URL#g" $ui_file
-    sed -i "s#https:\\\u002F\\\u002Fbeta-woudc-dev.cmc.ec.gc.ca/oapi#$WOUDC_UI_API_URL#g" $ui_file
+    sed -i "s#https:\\\u002F\\\u002Fgeobeta-woudc-dev.cmc.ec.gc.ca/oapi#$WOUDC_UI_API_URL#g" $ui_file
     sed -i "s#https:\\\u002F\\\u002Fbeta-woudc-dev.cmc.ec.gc.ca#$WOUDC_UI_WAF_URL#g" $ui_file
 done
 
