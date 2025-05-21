bb27a86f0c45db6eaabe83d37f0576e7e9fd36f4
Kevin Ngai <kngai@users.noreply.github.com>
2025-05-21 17:11:17 +0000

    Update changelog 2.0.0 - Update deploy/default files

--- deploy/default/update-vars.sh
+++ deploy/default/update-vars.sh
@@ -7,15 +7,15 @@ BASEDIR=/web/woudc-ui
 #Escaped characters in state.js
 for ui_file in `find $BASEDIR/dist -type f -name "state.js"`
 do
-    sed -i "s#https:\\\u002F\\\u002Fbeta-woudc-dev.cmc.ec.gc.ca#$WOUDC_UI_BASE_URL#g" $ui_file
-    sed -i "s#https:\\\u002F\\\u002Fgeobeta-woudc-dev.cmc.ec.gc.ca\\\u002Foapi#$WOUDC_UI_API_URL#g" $ui_file
-    sed -i "s#https:\\\u002F\\\u002Fbeta-woudc-dev.cmc.ec.gc.ca\\\u002Farchive#$WOUDC_UI_WAF_URL#g" $ui_file
+    sed -i "s#https:\\\u002F\\\u002Fwoudc.org#$WOUDC_UI_BASE_URL#g" $ui_file
+    sed -i "s#https:\\\u002F\\\u002Fapi.woudc.org\\\u002Foapi#$WOUDC_UI_API_URL#g" $ui_file
+    sed -i "s#https:\\\u002F\\\u002Fwoudc.org\\\u002Farchive#$WOUDC_UI_WAF_URL#g" $ui_file
 done
 
 #Other compiled .js files and the 200.html
 for ui_file in `find $BASEDIR/dist -type f -name "*.js" -o -name "200.html"`
 do
-    sed -i "s#https://beta-woudc-dev.cmc.ec.gc.ca#$WOUDC_UI_BASE_URL#g" $ui_file
-    sed -i "s#https://geobeta-woudc-dev.cmc.ec.gc.ca/oapi#$WOUDC_UI_API_URL#g" $ui_file
-    sed -i "s#https://beta-woudc-dev.cmc.ec.gc.ca/archive#$WOUDC_UI_WAF_URL#g" $ui_file
+    sed -i "s#https://woudc.org#$WOUDC_UI_BASE_URL#g" $ui_file
+    sed -i "s#https://api.woudc.org#$WOUDC_UI_API_URL#g" $ui_file
+    sed -i "s#https://woudc.org/archive#$WOUDC_UI_WAF_URL#g" $ui_file
 done
